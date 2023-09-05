c01b4e42697d6b823704777402e22bb7d3232fad
murphyjam <127764989+murphyjam@users.noreply.github.com>
2023-09-05 12:21:28 -0400

    final changes

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("your index.js is loaded correctly");
+$(document).ready(function() 
+{ 
+    $("#focusb").click(function() 
+    {
+         $("#flexContainer").slideDown("slow");
+})
+}
+)
